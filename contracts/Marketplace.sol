2937f7bfb93a5778a32c94c5f6a4e2974acda0c7
amy-amita <k.amitamy@gmail.com>
2023-02-17 19:48:57 +0700

    add function listNFT()

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -9,8 +9,31 @@ contract Marketplace is ReentrancyGuard{
     uint public immutable feePercent; // in case Keebit collect fee
     uint public itemCount;
 
+    struct NFT{
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool isSold;
+    }
+    mapping(uint => NFT) public NFTs;
+
     constructor(uint _feePercent){
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function listNFT(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant{
+        require(_price > 0, "Price must be at least 1 wei");
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        NFTs[itemCount] = NFT(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
     }
 }
