3cc40d7ddbb29df16d9a5637ed5faae288ac6237
amy-amita <k.amitamy@gmail.com>
2023-02-17 19:55:56 +0700

    edit variable names

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Marketplace is ReentrancyGuard{
+    // using Counters for Counters.Counter;
+    // Counters.Counter private nftCount;
     address payable public immutable feeAccount; // in case Keebit collect fee
     uint public immutable feePercent; // in case Keebit collect fee
     uint public itemCount;
@@ -17,7 +20,7 @@ contract Marketplace is ReentrancyGuard{
         address payable seller;
         bool isSold;
     }
-    mapping(uint => NFT) public NFTs;
+    mapping(uint => NFT) public nfts;
 
     constructor(uint _feePercent){
         feeAccount = payable(msg.sender);
@@ -27,7 +30,7 @@ contract Marketplace is ReentrancyGuard{
     function listNFT(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant{
         require(_price > 0, "Price must be at least 1 wei");
         _nft.transferFrom(msg.sender, address(this), _tokenId);
-        NFTs[itemCount] = NFT(
+        nfts[itemCount] = NFT(
             itemCount,
             _nft,
             _tokenId,
