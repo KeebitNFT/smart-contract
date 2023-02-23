046a6fab52b216cb80d76d52831b93e72308fb6a
amy-amita <k.amitamy@gmail.com>
2023-02-23 17:48:55 +0700

    add setApprovalForAll()

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,15 +6,17 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFT is ERC721URIStorage{
     uint public tokenCount;
+    address marketplaceContract;
 
-    constructor() ERC721("Keebit NFT", "Keebit"){
-
+    constructor(address _marketplaceContract) ERC721("Keebit NFT", "Keebit"){
+        marketplaceContract = _marketplaceContract;
     }
 
     function mint(string memory _tokenURI) external returns(uint){
         tokenCount++;
         _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, _tokenURI);
+        setApprovalForAll(marketplaceContract, true);
         return(tokenCount);
     }
 }
