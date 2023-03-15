8a5ceadf1f49d4f971c0c6a7f1400c5a6386baa7
amy-amita <k.amitamy@gmail.com>
2023-03-15 13:56:43 +0700

    remove name attribute from Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,14 +6,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Token is ERC1155, Ownable{
-    string[] public names; //string array of names
     uint[] public ids; //uint array of ids
     string public baseMetadataURI; //the token metadata URI
     string public tokenName; //the token mame
     uint public mintFee = 0 wei; //mintfee, 0 by default. only used in mint function, not batch.
-
-    mapping(string => uint) public nameToId; //name to id mapping
-    mapping(uint => string) public idToName; //id to name mapping
 
     // 1 contract = 1 collection
     // 1 collection can have multiple tokens, represented by names -> ids
@@ -21,24 +17,13 @@ contract Token is ERC1155, Ownable{
     constructor(
         string memory contractName, 
         string memory uri, 
-        string[] memory names, 
         uint[] memory ids
     )ERC1155(uri){
-        names = names;
         ids = ids;
-        _createMapping();
         setURI(uri);
         baseMetadataURI = uri;
         tokenName = contractName;
         transferOwnership(tx.origin);
-    }
-
-    function _createMapping() 
-    private{
-        for (uint id = 0; id < ids.length; id++) {
-            nameToId[names[id]] = ids[id];
-            idToName[ids[id]] = names[id];
-        }
     }
 
     function setURI(string memory uri) 
