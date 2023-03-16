d5864a34a5a1a3a532ebe45cf92246493eeb27d8
amy-amita <k.amitamy@gmail.com>
2023-03-16 23:03:15 +0700

    call listNFT() from factory

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -2,29 +2,37 @@
 pragma solidity ^0.8.17;
 
 import "./Token.sol";
+import "./Marketplace.sol";
 
 contract Factory{
 
+    Marketplace market;
     Token[] public tokens;
 
     event TokenDeployed(address owner, address tokenContract); //emitted when ERC1155 token is deployed
     event TokenMinted(address owner, address tokenContract, uint amount); //emmited when ERC1155 token is minted
 
-    function createNFT(
+    function createAndList(
         string memory _contractName, 
         string memory _uri, 
-        uint[] memory _ids
+        uint[] memory _ids,
+        uint _price
     )
     public
     returns(address){
         // deploy contract
-        Token t = new Token(_contractName, _uri, _ids);
-        tokens.push(t);
-        emit TokenDeployed(msg.sender,address(t));
+        Token tokenContract = new Token(_contractName, _uri, _ids);
+        tokens.push(tokenContract);
+        emit TokenDeployed(msg.sender,address(tokenContract));
 
         //mint NFT
-        _mintNFT(t, _ids);
-        return address(t);
+        _mintNFT(tokenContract, _ids);
+
+        //list NFT
+        for(uint i=0; i<_ids.length; i++){
+            market.listNFT(tokenContract, _ids[i], _price);
+        }
+        return address(tokenContract);
 
     }
 
