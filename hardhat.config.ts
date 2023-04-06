44760369966368631ef011797464ecfe8edb563b
James <jameskrap123@gmail.com>
2023-04-06 22:06:46 +0700

    add abi

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-abi-exporter';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  abiExporter: {
+    runOnCompile: true,
+    clear: true,
+    pretty: true,
+  },
 };
 
 export default config;
