e324c293ba8fd2cda39ce9999a3867680960a5b2
James <jameskrap123@gmail.com>
2023-04-07 00:22:15 +0700

    change abi settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,13 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-abi-exporter";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-abi-exporter';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
   abiExporter: {
     runOnCompile: true,
     clear: true,
-    pretty: true,
+    flat: true,
   },
 };
 
